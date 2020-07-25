e865999e06b88ce92d4ee8ed7234f26edcc78107
Abhinavab <abhinavav23@gmail.com>
2020-07-25 15:03:37 +0530

    changed city model

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/weather-application'));
+app.use(express.static(__dirname + "./dist/weather-application"));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/weather-application/index.html'));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "./dist/weather-application/index.html"));
 });
 
 app.listen(process.env.PORT || 8080);
